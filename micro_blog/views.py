d057118bf83970f5e07ca8ea20f402cd68b28618
santharao <santharao@micropyramid.com>
2017-04-19 19:13:27 +0530

    django service page added

--- micro_blog/views.py
+++ micro_blog/views.py
@@ -35,7 +35,7 @@ from .tasks import *
 #     return render(request, 'admin/browse.html', {'files': imgs})
 
 def empty(request):
-    return render(request, 'site/empty.html')
+    return render(request, 'site/django_service.html')
 
 
 @login_required
