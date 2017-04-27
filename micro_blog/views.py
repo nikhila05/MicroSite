58d407713a0d3e09d0f37b515169c91443eaf79b
santharao <santharao@micropyramid.com>
2017-04-27 12:47:10 +0530

    python  service page created

--- micro_blog/views.py
+++ micro_blog/views.py
@@ -35,7 +35,7 @@ from .tasks import *
 #     return render(request, 'admin/browse.html', {'files': imgs})
 
 def empty(request):
-    return render(request, 'site/django_service.html')
+    return render(request, 'site/python_service.html')
 
 
 @login_required
