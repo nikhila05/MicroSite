dc7c37edeee0f29fdcb0a6e66991d08b96283322
santharao <santharao@micropyramid.com>
2017-05-05 16:40:29 +0530

    testing_service_page_finished

--- micro_blog/views.py
+++ micro_blog/views.py
@@ -35,7 +35,7 @@ from .tasks import *
 #     return render(request, 'admin/browse.html', {'files': imgs})
 
 def empty(request):
-    return render(request, 'site/python_service.html')
+    return render(request, 'site/testing_service.html')
 
 
 @login_required
